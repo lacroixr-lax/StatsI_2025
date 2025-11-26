edcf825d20b735e108e40bab8159102a133f39f5
lacroixr-lax <lacroixr@tcd.ie>
2025-11-26 13:04:12 +0000

    Tutorial

--- problemSets/PS04/my_answers/PS04_RL.R
+++ problemSets/PS04/my_answers/PS04_RL.R
@@ -61,7 +61,9 @@ stargazer(model1,
 # Calculate the change in ˆ y associated with a $1,000 increase in income based 
 # on your answer for (c).
   # (c) y = 21.142 + 0.003income + 37.781prof - 0.002income*prof
-21.142 + 0.003*(1000) + 37.781*(1) - 0.002*(1000*1)
+income_0 <- 21.142 + 37.781
+income_1000 <- 21.142 + 0.003*(1000) + 37.781*(1) - 0.002*(1000*1)
+income_1000 - income_0
 
 # (g) What is the eﬀect of changing one’s occupations from non-professional to 
 # professional when her income is $6,000? We are interested in the marginal 
@@ -78,13 +80,35 @@ prof - non_prof
 beta1 <- 0.042
 SE1 <- 0.016
 n1 <- 30
+# Number of variables
+k1 <- 1
+# Calculate t-statistic
 TS1 <- (beta1 - 0)/SE1
-p_value1 <- 2*pt(abs(TS1), n1-1, lower.tail = F)
+# Calculate p-value for test statistic
+p_value1 <- 2*pt(abs(TS1), n1-k1-1, lower.tail = F)
 
 # (b) Use the results to determine whether being next to precincts with these 
 # yard signs aﬀects vote share (e.g., conduct a hypothesis test with α=.05).
 beta2 <- 0.042
 SE2 <- 0.013
 n2 <- 76
+# Number of variables
+k2 <- 1
+# Calculate t-statistic
 TS2 <- (beta2 - 0)/SE2
-p_value2 <- 2*pt(abs(TS2), n2-1, lower.tail = F)
+# Calculate p-value for test statistic
+p_value2 <- 2*pt(abs(TS2), n2-k2-1, lower.tail = F)
+
+# (d) Evaluate the model fit for this regression. What does this tell us about
+# the importance of yard signs versus the other factors that are not modeled?
+r_squared <- 0.094
+n <- 131
+# k = number of variables
+k <- 2
+# F statistic using the r_squared value
+F_test <- (r_squared/(k-1))/((1-r_squared)/(n-k))
+# F test requires 2 degrees of freedom
+df1 <- k-1
+df2 <- n-k 
+# Calculate p-value for test statistic
+F_pvalue <- pf(F_test, df1, df2)
